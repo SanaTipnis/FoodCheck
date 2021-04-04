df0d50ca37ba9dd4ff7de543cf4675d3b1b3c03e
Sana Tipnis <sat5652@psu.edu>
2021-04-04 13:21:42 -0400

    test1

--- FoodCheck/AppDelegate.swift
+++ FoodCheck/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "PZnRaQQA9qVwfk1LMhruP1wypwyR8aGEoltvYh2s"
+            $0.clientKey = "g7bTrkFwJzOqsDvvSQNiaujambF7pyD6jvPRC2UO"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        
         return true
     }
 
